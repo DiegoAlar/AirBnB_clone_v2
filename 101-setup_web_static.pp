5f95e6cfbb59be320678f266c2e74c94ce203366
DiegoAlar <diegoaav@hotmail.com>
2020-04-08 15:13:22 -0500

    adding task 101

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,5 @@
+# Bash script that sets up your web servers for the deployment of web_static
+exec { 'setup_server_for_deploy':
+command  => 'sudo apt-get update && sudo apt-get install -y nginx && sudo mkdir -p /data/ && sudo mkdir -p /data/web_static && sudo mkdir -p /data/web_static/shared && sudo mkdir -p /data/web_static/releases && sudo mkdir -p /data/web_static/releases/test && sudo touch /data/web_static/releases/test/index.html && echo "Configuration went well!" | sudo tee /data/web_static/releases/test/index.html && sudo ln -sf /data/web_static/releases/test/ /data/web_static/current && sudo chown -R ubuntu:ubuntu /data/ && sed -i "/pass the PHP/i \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}" /etc/nginx/sites-enabled/default && sudo service nginx restart',
+provider => shell,
+}
