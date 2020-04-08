69702fb01dc71594bc05431a218bdd42c2b25b14
DiegoAlar <diegoaav@hotmail.com>
2020-04-08 15:01:08 -0500

    fixing 100

---
+++ 100-clean_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" deletes out-of-date archives, using the function do_clean """
+
+
+from fabric.api import *
+
+
+env.hosts = ['35.237.89.95', '3.80.128.164']
+
+
+def do_clean(number=0):
+    """ function to clean local and servers """
+    path_local = 'versions'
+    path_servers = "/data/web_static/releases"
+    num = eval(number)
+    if number == 0 or number == 1:
+        new_number = 2
+    else:
+        new_number = eval(number) + 1
+    with lcd(path_local):
+        local("ls -1t | grep -v '/$' | tail -n +{:d} | xargs rm -rf".format(new_number))
+    with cd(path_servers):
+        sudo("ls -1t -I test | tail -n +{:d} | xargs rm -rf".format(new_number))
