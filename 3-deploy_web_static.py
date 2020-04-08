e134b0fd51cecf1a750ce690b2a7896da93a668c
DiegoAlar <diegoaav@hotmail.com>
2020-04-08 09:22:42 -0500

    fixing 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,8 +5,6 @@
 
 import os
 from fabric.api import *
-# from 1-pack_web_static import do_pack
-# from 2-do_deploy_web_static import do_deploy
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
