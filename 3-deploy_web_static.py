cbb56b1c355032618cbbd2f1884312711b860956
DiegoAlar <diegoaav@hotmail.com>
2020-04-09 15:03:59 -0500

    fixing 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -8,6 +8,9 @@ do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
 
+env.hosts = ['35.237.89.95', '3.80.128.164']
+
+
 def deploy():
     """ that creates and distributes an archive to your web servers """
     path = do_pack()
