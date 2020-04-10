84910d273e0ea3cf1a701db90a1f9d28ba82d3ae
DiegoAlar <diegoaav@hotmail.com>
2020-04-10 12:17:41 -0500

    fixing 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -4,21 +4,11 @@
  """
 
 from fabric.api import *
-# do_pack = __import__('1-pack_web_static').do_pack
-# do_deploy = __import__('2-do_deploy_web_static').do_deploy
-
-# def deploy():
-#     """ that creates and distributes an archive to your web servers """
-#     path = do_pack()
-#     if path:
-#         return do_deploy(path)
-#     else:
-#         return False
-
 from datetime import datetime
 import os
 
 env.hosts = ['35.237.89.95', '3.80.128.164']
+
 
 @task
 def deploy():
@@ -28,6 +18,7 @@ def deploy():
         return do_deploy(path)
     else:
         return False
+
 
 @runs_once
 def do_pack():
@@ -42,6 +33,7 @@ def do_pack():
         print("web_static packed: {} -> {}Bytes".format(path, size))
         return path
     return None
+
 
 def do_deploy(archive_path):
     """ sends file to server """
@@ -67,5 +59,3 @@ def do_deploy(archive_path):
             return True
     else:
         False
-
-    
