4ba1f089ca729e9cc0c68f1f3bd5169d271691ab
DiegoAlar <diegoaav@hotmail.com>
2020-04-10 16:25:10 -0500

    fixing 100

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,12 +12,15 @@ def do_clean(number=0):
     """ function to clean local and servers """
     path_local = 'versions'
     path_servers = "/data/web_static/releases"
-    num = eval(number)
-    if number == 0 or number == 1:
-        new_number = 1
-    elif number > 1:
-        new_number = eval(number) + 1
+    num = int(number)
+    if num == 0 or num == 1:
+        new_number = 2
+    elif num > 1:
+        new_number = num + 1
     with lcd(path_local):
-        local("ls -1t | grep -v '/$' | tail -n +{:d} | xargs rm -rf".format(new_number))
+        local(
+            "ls -1t | grep -v '/$' | tail -n +{:d} | xargs rm -rf".format(
+                new_number))
     with cd(path_servers):
-        sudo("ls -1t -I test | tail -n +{:d} | xargs rm -rf".format(new_number))
+        sudo(
+            "ls -1t -I test | tail -n +{:d} | xargs rm -rf".format(new_number))
