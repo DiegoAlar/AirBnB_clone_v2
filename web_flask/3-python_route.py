18ab9204d3184f21a1e43e38bbb47efd04f6ac35
DiegoAlar <diegoaav@hotmail.com>
2020-04-21 12:33:49 -0500

    adding tasks from 0 to 6

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """ Returns a message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_route2():
+    """ Returns a message """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def hello_route3(text):
+    """ display “C ” followed by the value of the text variable """
+    return 'C {}'.format(text.replace('_', ' '))
+    # return 'C %s' % text.replace('_', ' ')
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def hello_route4(text='is cool'):
+    """ display “Python ” followed by the value of the text variable """
+    return 'Python {}'.format(text.replace('_', ' '))
+    # return 'Python %s' % text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
