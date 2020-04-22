90b2c733ce625fe12d3bee5c9a0b63caedf8b08b
DiegoAlar <diegoaav@hotmail.com>
2020-04-21 19:36:12 -0500

    fixing 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,6 +6,7 @@ script that starts a Flask web application
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ def teardown_request(self):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """ display all states """
-    all_states = storage.all('State')
+    all_states = storage.all(State)
     return render_template('7-states_list.html', all_states)
 
 
